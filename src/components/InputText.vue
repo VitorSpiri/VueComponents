786f4767405c6b56233c045e5588a9dbef53476e
Vitor Santos <vitorspiri1@gmail.com>
2024-11-02 19:26:47 -0300

    feat: configuração de props e emit InputText component

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
+import { defineProps, defineEmits, ref } from 'vue'
+
+const props = defineProps({
+    title: { type: String, required: true },
+    inputValue: {type: String, required: true}
+})
+
+const emit = defineEmits(['update:inputValue'])
+
+const localInputValue = ref(props.inputValue)
+
+const updateValue = (value: string) => {
+    localInputValue.value = value
+    emit('update:inputValue', value)
+}
 
 </script>
 
 <template>
-    <input v-model="oi" />
+    <label>{{props.title}}</label>
+    <input v-model="props.inputValue" />
 </template>
 
 <style scoped>
